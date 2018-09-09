cf2176a74909f6eaa01a6944569241be951ba7e1
Ruslan Myaskovskiy <russ.myaskovskiy@gmail.com>
2018-09-09 00:42:17 -0700

    the commit

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+-- Select burgers_db
+USE `burgers_db`;
+
+-- insert 3 burgers
+INSERT INTO `burgers` (burger_name, devoured, date) VALUES ('Chili', false, CURRENT_TIMESTAMP);
+INSERT INTO `burgers` (burger_name, devoured, date) VALUES ('Old Timer', false, CURRENT_TIMESTAMP);
+INSERT INTO `burgers` (burger_name, devoured, date) VALUES ('Guacamole', false, CURRENT_TIMESTAMP);
+INSERT INTO `burgers` (burger_name, devoured, date) VALUES ('Southern Smokehouse', false, CURRENT_TIMESTAMP);
